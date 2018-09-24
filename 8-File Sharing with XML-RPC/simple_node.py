654800650b66e8020f2ae2aac0868f9f8da4b82b
Neil <2855913433@qq.com>
2018-09-24 14:05:27 +0800

    Add files via upload

---
+++ 8-File Sharing with XML-RPC/simple_node.py
@@ -0,0 +1,101 @@
+from xmlrpc.client import ServerProxy
+from os.path import join, isfile
+from xmlrpc.server import SimpleXMLRPCServer
+from urllib.parse import urlparse
+import sys
+
+MAX_HISTORY_LENGTH = 6  # 访问链最大长度
+
+OK = 1  # 查询状态正常
+FAIL = 2  # 查询状态无效
+EMPTY = ''
+
+def get_port(url):
+    '从URL中提取端口'
+    name = urlparse(url)[1]  # 解析并获取URL中[域名:端口]
+    parts = name.split(':')  # 获取以分号结束最后一组
+    return int(parts[-1])
+
+
+class Node:
+    """
+    P2P网络中的节点
+    """
+    def __init__(self, url, dirname, secret):
+        self.url = url
+        self.dirname = dirname
+        self.secret = secret
+        self.known = set()
+
+    def query(self, query, history=[]):
+        """
+        查询文件（可能向已知节点寻求帮助），并以字符串的方式返回它
+        """
+        code, data = self._handle(query)  # 获取处理请求的结果
+        if code == OK:  # 如果正常状态
+            return code, data  # 返回状态和数据
+        else:
+            history = history + [self.url]  # 历史记录添加已请求过的节点
+            if len(history) >= MAX_HISTORY_LENGTH:  # 如果历史请求超过六次
+                return FAIL, EMPTY  # 反返回无效状态和空数据
+            return self._broadcast(query, history)  # 返回广播结果
+
+    def hello(self, other):
+        """
+        用于向其他节点介绍当前节点
+        """
+        self.known.add(other)
+        return OK
+
+    def fetch(self, query, secret):
+        """
+        用于让节点查找并下载文件
+        """
+        if secret != self.secret: return FAIL  # 匹配密钥
+        code, data = self.query(query)  # 处理请求获取文件状态与数据
+        if code == OK:
+            f = open(join(self.dirname, query), 'w')  # 写入模式打开
+            f.write(data)  # 将获取到的数据写入文件
+            f.close()
+            return OK
+        else:
+            return FAIL
+
+    def _start(self):
+        """
+        供内部用来启动XML-RPC服务器
+        """
+        s = SimpleXMLRPCServer(("", get_port(self.url)), logRequests=False)
+        s.register_instance(self)  # 注册类的实例到服务器对象
+        s.serve_forever()
+
+    def _handle(self, query):
+        """
+        供内部用来处理查询
+        """
+        dir = self.dirname
+        name = join(dir, query)  # 获取请求路径
+        if not isfile(name): return FAIL, EMPTY  # 判断路径是否文件
+        return OK, open(name).read()
+
+    def _broadcast(self, query, history):
+        """
+        供内部用来向所有已知节点广播查询
+        """
+        for other in self.known.copy():  # 遍历已知节点的列表
+            if other in history: continue  # 如果已知节点存在于历史记录继续下一节点
+            try:
+                s = ServerProxy(other)  # 访问非历史记录中的已知节点
+                code, data = s.query(query, history)  # 向已知节点发出请求
+                if code == OK:  # 如果状态为正常
+                    return code, data  # 返回有效状态和数据
+            except:
+                self.known.remove(other)  # 如果发生异常，从已知节点列表移除节点
+        return FAIL, EMPTY  # 返回无效状态和空数据
+
+def main():
+    url, directory, secret = sys.argv[1:]
+    n = Node(url, directory, secret)
+    n._start()
+
+if __name__ == '__main__': main()
---
+++ 8-File Sharing with XML-RPC/user.md
@@ -0,0 +1,46 @@
+#尝试使用
+打开多个终端， 创建目录files1和files2，在files2下创建文件test.txt，再在一个终端执行命令：
+```python
+python simple_node.py http://127.0.0.1:4242 files1 123456
+```
+再创建一个对等体，在另一个终端执行命令：
+```python
+python simple_node.py http://127.0.0.1:4243 files2 456789
+```
+启动python交互解释器，尝试连接到其中一个对等体：
+```python
+>>> from xmlrpc.client import *
+>>> mypeer = ServerProxy('http://127.0.0.1:4242')
+>>> code, data = mypeer.query('test.txt')
+>>> code
+2
+```
+很明显失败了，我们尝试连接另一个对等体：
+```python
+>>> otherpeer = ServerProxy('http://127.0.0.1:4243')
+>>> code, data = otherpeer.query('test.txt')
+>>> code
+1
+```
+查询成功，因为files2目录下有test.txt文件。如果test.txt文件包含文本不多，可显示变量data的内容核实传输内容正确：
+```python
+>>> data
+'This is a test\n'
+```
+向第二个对等体介绍第一个对等体：
+```python
+>>> mypeer.hello('http://127.0.0.1:4243')
+1
+```
+现在，第一个对等体知道第二个对等体的URL，可向其寻求帮助了。再次查询第一个对等体：
+```python
+>>> mypeer.query('test.txt')
+[1, 'This is a test\n']
+```
+成功了！
+现在我们测试最后一项功能----让节点一从节点二下载文件并存储它：
+```python
+>>> mypeer.fetch('test.txt','123456')
+1
+```
+你会发现，文件test.txt神奇地出现在files1目录下。
